bbb0912880aa3f8a1977f20b10fe330e6589a62b
RollowO <john_liong@dlsu.edu.ph>
2024-07-30 02:15:32 +0800

    To dos for mco

--- CODE/HotelController.java
+++ CODE/HotelController.java
@@ -2,8 +2,16 @@
 import java.util.ArrayList;
 /*
 TODO: 
+      1.low-level info about selected reservation doesn't apply the discount so total price & revenue of hotel is diff even if shld be same,
+      total revenue of hotel is correct
+      2. Maybe display rooms that has removable reservation and the name of the guest?
+      3. when reserving then modifying date price its not changing, idk if we need to fix that tho(idt needed since also this logic if
+      we update base price of rooms n totalprice isnt changing)
+      4. Modify date price isnt affecting the total price displayed on resrvation info and High-level info for total revenue in hotel
+      5. Ur not supposed to be able to remove a hotel when it has any reservations
 
 
+-------------------------------------------------------------
       5. For booking rooms, hotel name shld be displayed for number to be selected
       6. For Removing reservations, display rooms that can be removed
       7. After doing all these changes, try the methods that uses rooms again cuz addRoom, bookRoom, removeReservation, etc.
